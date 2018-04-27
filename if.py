efeacc2be709cee1fd5b9f04d1d6c11885ec871f
magia100 <magia100@gmail.com>
2018-04-27 04:27:06 -0400

    nn

---
+++ if.py
@@ -0,0 +1,20 @@
+x = int(input("Please enter an integer: "))
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+     print('Zero')
+elif x == 1:
+     print('Single')
+else:
+     print('More')
+
+
+words = ['cat','dog','window','defenestrate']
+print ("words: ", words)
+for w in words[:]:
+    if len(w) > 6:
+        words.insert(0,w)
+
+print ("words2: ", words)
+       
